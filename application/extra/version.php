56dfdca4344420f25d9496719f10ad2fa378ed19
maccmspro <info@maccms.pro>
2021-06-29 23:26:50 +0800

    update to  2021.1000.2000

--- application/extra/version.php
+++ application/extra/version.php
@@ -2,8 +2,8 @@
 return array (
     'name' => '苹果CMS内容管理系统',
     'copyright' => 'MacCMS',
-    'url' => '//github.com/maccmspro/maccms10',
-    'code' => '2021.1000.1090',
+    'url' => '//github.com/maccmspro',
+    'code' => '2021.1000.2000',
     'license' => '免费版',
 );
 ?>
