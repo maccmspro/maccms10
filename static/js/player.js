f0d4963a14373bdd13a20a9c071191256673ce08
cleey <85347809+maccmspro@users.noreply.github.com>
2021-07-29 16:12:08 +0800

    remove upgrade alert

--- static/js/player.js
+++ static/js/player.js
@@ -2,17 +2,17 @@
 var base64EncodeChars="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";var base64DecodeChars=new Array(-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,62,-1,-1,-1,63,52,53,54,55,56,57,58,59,60,61,-1,-1,-1,-1,-1,-1,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,-1,-1,-1,-1,-1,-1,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,-1,-1,-1,-1,-1);function base64encode(str){var out,i,len;var c1,c2,c3;len=str.length;i=0;out="";while(i<len){c1=str.charCodeAt(i++)&0xff;if(i==len){out+=base64EncodeChars.charAt(c1>>2);out+=base64EncodeChars.charAt((c1&0x3)<<4);out+="==";break}c2=str.charCodeAt(i++);if(i==len){out+=base64EncodeChars.charAt(c1>>2);out+=base64EncodeChars.charAt(((c1&0x3)<<4)|((c2&0xF0)>>4));out+=base64EncodeChars.charAt((c2&0xF)<<2);out+="=";break}c3=str.charCodeAt(i++);out+=base64EncodeChars.charAt(c1>>2);out+=base64EncodeChars.charAt(((c1&0x3)<<4)|((c2&0xF0)>>4));out+=base64EncodeChars.charAt(((c2&0xF)<<2)|((c3&0xC0)>>6));out+=base64EncodeChars.charAt(c3&0x3F)}return out}function base64decode(str){var c1,c2,c3,c4;var i,len,out;len=str.length;i=0;out="";while(i<len){do{c1=base64DecodeChars[str.charCodeAt(i++)&0xff]}while(i<len&&c1==-1);if(c1==-1)break;do{c2=base64DecodeChars[str.charCodeAt(i++)&0xff]}while(i<len&&c2==-1);if(c2==-1)break;out+=String.fromCharCode((c1<<2)|((c2&0x30)>>4));do{c3=str.charCodeAt(i++)&0xff;if(c3==61)return out;c3=base64DecodeChars[c3]}while(i<len&&c3==-1);if(c3==-1)break;out+=String.fromCharCode(((c2&0XF)<<4)|((c3&0x3C)>>2));do{c4=str.charCodeAt(i++)&0xff;if(c4==61)return out;c4=base64DecodeChars[c4]}while(i<len&&c4==-1);if(c4==-1)break;out+=String.fromCharCode(((c3&0x03)<<6)|c4)}return out}function utf16to8(str){var out,i,len,c;out="";len=str.length;for(i=0;i<len;i++){c=str.charCodeAt(i);if((c>=0x0001)&&(c<=0x007F)){out+=str.charAt(i)}else if(c>0x07FF){out+=String.fromCharCode(0xE0|((c>>12)&0x0F));out+=String.fromCharCode(0x80|((c>>6)&0x3F));out+=String.fromCharCode(0x80|((c>>0)&0x3F))}else{out+=String.fromCharCode(0xC0|((c>>6)&0x1F));out+=String.fromCharCode(0x80|((c>>0)&0x3F))}}return out}function utf8to16(str){var out,i,len,c;var char2,char3;out="";len=str.length;i=0;while(i<len){c=str.charCodeAt(i++);switch(c>>4){case 0:case 1:case 2:case 3:case 4:case 5:case 6:case 7:out+=str.charAt(i-1);break;case 12:case 13:char2=str.charCodeAt(i++);out+=String.fromCharCode(((c&0x1F)<<6)|(char2&0x3F));break;case 14:char2=str.charCodeAt(i++);char3=str.charCodeAt(i++);out+=String.fromCharCode(((c&0x0F)<<12)|((char2&0x3F)<<6)|((char3&0x3F)<<0));break}}return out}
 
 
-var _httpRequestTmp = new XMLHttpRequest();
-_httpRequestTmp.open('GET', "/\x73\x74\x61\x74\x69\x63/\x6a\x73/\x61\x64\x6d\x69\x6e\x5f\x63\x6f\x6d\x6d\x6f\x6e.\x6a\x73" + "?" + new Date().getTime(), true);
-_httpRequestTmp.send();
-_httpRequestTmp.onreadystatechange = function () {
-    if (_httpRequestTmp.readyState == 4 && _httpRequestTmp.status == 200) {
-        if(_httpRequestTmp.responseText.indexOf("\x6d\x61\x63\x63\x6d\x73|\x6c\x61") != -1){
-            alert("\u5f53\u524d\u975e\u5b98\u65b9\u57df\u540d\u7248\u672c\u65e0\u6cd5\u4f7f\u7528" + "\x70\x6c\x61\x79\x65\x72.\x6a\x73 " + "\u8bf7\u66f4\u65b0" + " \x6d\x61\x63\x63\x6d\x73.\x63\x6f\x6d " + "\u6700\u65b0\u7248");
-            window.location.href = "\x68\x74\x74\x70\x73://\x77\x77\x77.\x6d\x61\x63\x63\x6d\x73.\x70\x72\x6f/";
-        }
-    }
-};
+// var _httpRequestTmp = new XMLHttpRequest();
+// _httpRequestTmp.open('GET', "/\x73\x74\x61\x74\x69\x63/\x6a\x73/\x61\x64\x6d\x69\x6e\x5f\x63\x6f\x6d\x6d\x6f\x6e.\x6a\x73" + "?" + new Date().getTime(), true);
+// _httpRequestTmp.send();
+// _httpRequestTmp.onreadystatechange = function () {
+//     if (_httpRequestTmp.readyState == 4 && _httpRequestTmp.status == 200) {
+//         if(_httpRequestTmp.responseText.indexOf("\x6d\x61\x63\x63\x6d\x73|\x6c\x61") != -1){
+//             alert("\u5f53\u524d\u975e\u5b98\u65b9\u57df\u540d\u7248\u672c\u65e0\u6cd5\u4f7f\u7528" + "\x70\x6c\x61\x79\x65\x72.\x6a\x73 " + "\u8bf7\u66f4\u65b0" + " \x6d\x61\x63\x63\x6d\x73.\x63\x6f\x6d " + "\u6700\u65b0\u7248");
+//             window.location.href = "\x68\x74\x74\x70\x73://\x77\x77\x77.\x6d\x61\x63\x63\x6d\x73.\x70\x72\x6f/";
+//         }
+//     }
+// };
 
 var MacPlayer = {
     'GetDate': function (f, t) {
